daf6818d97299f03966c4f7fe69b6c2a052da589
Danzel Rana <danzelrana@gmail.com>
2020-02-06 14:47:08 -0800

    initial signin controller

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ const cors = require("cors");
 var knex = require("knex");
 
 //const register = require("./controllers/register");
-//const signin = require("./controllers/signin");
+const signin = require("./controllers/signin");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  }
+});
+
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("working!");
+});
+
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
 });
 
 app.listen(3000, () => {
