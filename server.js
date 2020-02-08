1b275477f60f4a7f50f14ba863da8bc82d5c66de
Danzel Rana <danzelrana@gmail.com>
2020-02-07 16:21:41 -0800

    added register controller

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const bcrypt = require("bcryptjs");
 const cors = require("cors");
 var knex = require("knex");
 
-//const register = require("./controllers/register");
+const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 
 const db = knex({
   client: "pg",
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "secret",
+    database: "anime-watch"
+    //connectionString: process.env.DATABASE_URL,
+    //ssl: true
   }
 });
 
@@ -25,6 +29,10 @@ app.get("/", (req, res) => {
 
 app.post("/signin", (req, res) => {
   signin.handleSignin(req, res, db, bcrypt);
+});
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 app.listen(3000, () => {
