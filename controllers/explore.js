079a74a9af988fe09ecb1e5f648dc9e28885961d
Danzel Rana <danzelrana@gmail.com>
2020-06-14 09:59:16 -0700

    readded jikanjs wrapper

--- controllers/explore.js
+++ controllers/explore.js
@@ -1,8 +1,8 @@
+const jikanjs = require("jikanjs");
+
 const handleApiCall = async (req, res) => {
   try {
-    const results = await fetch(
-      `https://api.jikan.moe/v3/search/anime?q=${req.body.input}&page=1`
-    );
+    const results = await jikanjs.search("anime", req.body.input);
     return res.json(results);
   } catch (err) {
     res.status(400).json("unable to work with API");
